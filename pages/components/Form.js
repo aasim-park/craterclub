2bb6c24bb9590a41318c4faaf8e897a64d91887f
Aasim Shaikh <asimshaikh1993@gmail.com>
2022-04-23 10:27:55 +0530

    basic setup

---
+++ pages/components/Form.js
@@ -0,0 +1,17 @@
+import { Formik, Form } from "formik";
+
+const SignUp = () => {
+  return (
+    <>
+      <Formik
+      initialValues={
+          
+      }>{(formik) => <div>Trial form</div>}</Formik>
+      <Form>
+
+      </Form>
+    </>
+  );
+};
+
+export default SignUp;
